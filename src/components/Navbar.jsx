d4bda96b8cd7d76c06fb44f9453adcb53b8d1b86
Adarsh Paswan <112092468+Adarsh130@users.noreply.github.com>
2026-07-31 19:59:05 +0530

    Update import paths for logo images in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import bitLogo from "/src/assets/BIT-LOGO1.png";
-import ieeelogo from "/src/assets/IEEE-logo.JPG";
+import bitLogo from "../assets/BIT-LOGO1.png";
+import ieeelogo from "../assets/IEEE-logo.JPG";
 
 function Navbar() {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
